ead643b014c9618101dda5f27a8bf325e33de30a
Alice Lee <alicejeeuncodes@gmail.com>
2022-11-18 22:32:25 +0000

    Set up parse route for app.

--- client/app.jsx
+++ client/app.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import Home from './pages/home';
+import parseRoute from './lib/parse-route';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      route: parseRoute(window.location.hash)
+    };
+  }
+
+  // renderPage() {
+  //   const { path } = this.state.route;
+  //   if (path === 'home' || path === '') {
+  //     return <Home state={this.state} />;
+  //   }
+  // }
+
   render() {
-    return <Home />;
+    return <Home/>;
   }
 }
